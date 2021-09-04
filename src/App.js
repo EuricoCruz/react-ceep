38e0340e8a0b997522435a39483d5f07cc83f3f8
EuricoCruz <euricoscruz@hotmail.com>
2021-09-04 13:31:05 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import ListaDeNotas from "./components/ListaDeNotas"
 
 function App() {
   return (
-    <h1>Meu app</h1>    
+    <section>
+      <form>
+        <input type='text' placeholder='Título'/>
+        <textarea placeholder='Escreva sua nota'/>
+        <button>Criar Nota</button>
+      </form>  
+      <ListaDeNotas/>
+    </section>  
   );
 }
 
